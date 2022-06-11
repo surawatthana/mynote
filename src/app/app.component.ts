72dad45864fab71fabab7844ec96e4c81a7750cf
Sura Watthanalamlert <surawatthana@hotmail.com>
2022-06-11 16:08:06 +0700

    clean panel menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { MegaMenuItem, MenuItem } from 'primeng/api';
 export class AppComponent  implements OnInit, OnDestroy {
   title = 'my-note-app';
 
-  panleMenuHeightMargin = 80;
+  panelMenuHeightMargin = 80;
 
   editor!: Editor;
   html!: '';
@@ -34,13 +34,13 @@ export class AppComponent  implements OnInit, OnDestroy {
 
   innerWidth: any;
   innerHeight: any;
-  panleMenuHeight: string = 700 + 'px';
+  panelMenuHeight: string = 700 + 'px';
 
 
   ngOnInit() {
     this.innerWidth = window.innerWidth;
     this.innerHeight = window.innerHeight;
-    this.panleMenuHeight = (this.innerHeight - this.panleMenuHeightMargin) +  'px';
+    this.panelMenuHeight = (this.innerHeight - this.panelMenuHeightMargin) +  'px';
     this.editor = new Editor();
     this.html = '';
     this.items = [
@@ -179,112 +179,7 @@ export class AppComponent  implements OnInit, OnDestroy {
       items: [
           {
               label: 'New',
-              icon:'pi pi-fw pi-plus',
-              items: [
-                  {
-                  label: 'Bookmark',
-                  icon:'pi pi-fw pi-bookmark'
-                  },
-                  {
-                  label: 'Video',
-                  icon:'pi pi-fw pi-video'
-                  }
-              ]
-          },
-          {
-              label: 'Delete',
-              icon:'pi pi-fw pi-trash'
-          },
-          {
-              label: 'Export',
-              icon:'pi pi-fw pi-external-link'
-          }
-      ]
-      },
-      {
-      label: 'Edit',
-      icon:'pi pi-fw pi-pencil',
-      items: [
-          {
-              label: 'Left',
-              icon:'pi pi-fw pi-align-left'
-          },
-          {
-              label: 'Right',
-              icon:'pi pi-fw pi-align-right'
-          },
-          {
-              label: 'Center',
-              icon:'pi pi-fw pi-align-center'
-          },
-          {
-              label: 'Justify',
-              icon:'pi pi-fw pi-align-justify'
-          }
-      ]
-      },
-      {
-      label: 'Users',
-      icon:'pi pi-fw pi-user',
-      items: [
-          {
-              label: 'New',
-              icon:'pi pi-fw pi-user-plus',
-
-          },
-          {
-              label: 'Delete',
-              icon:'pi pi-fw pi-user-minus',
-          },
-          {
-              label: 'Search',
-              icon:'pi pi-fw pi-users',
-              items: [
-                  {
-                  label: 'Filter',
-                  icon:'pi pi-fw pi-filter',
-                  items: [
-                      {
-                          label: 'Print',
-                          icon:'pi pi-fw pi-print'
-                      }
-                  ]
-                  },
-                  {
-                  icon:'pi pi-fw pi-bars',
-                  label: 'List'
-                  }
-              ]
-          }
-      ]
-      },
-      {
-      label: 'Events',
-      icon:'pi pi-fw pi-calendar',
-      items: [
-          {
-              label: 'Edit',
-              icon:'pi pi-fw pi-pencil',
-              items: [
-                  {
-                  label: 'Save',
-                  icon:'pi pi-fw pi-calendar-plus'
-                  },
-                  {
-                  label: 'Delete',
-                  icon:'pi pi-fw pi-calendar-minus'
-                  }
-              ]
-          },
-          {
-              label: 'Archieve',
-              icon:'pi pi-fw pi-calendar-times',
-              items: [
-                  {
-                  label: 'Remove',
-                  icon:'pi pi-fw pi-calendar-minus'
-                  }
-              ]
+              icon:'pi pi-fw pi-plus'
           }
       ]
       }
@@ -300,6 +195,6 @@ export class AppComponent  implements OnInit, OnDestroy {
   onResize(event: any) {
     this.innerWidth = window.innerWidth;
     this.innerHeight = window.innerHeight;
-    this.panleMenuHeight = (this.innerHeight - this.panleMenuHeightMargin) +  'px';
+    this.panelMenuHeight = (this.innerHeight - this.panelMenuHeightMargin) +  'px';
   }
 }
